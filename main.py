af7879787667313a7092f916a4c7334e7f2c59ba
rtanoeiro <ramontanoeiro@gmail.com>
2025-03-23 20:32:35 +1100

    updated main.py to parte arguments from shell

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 from pantry.pantry import Pantry
 from pantry.pantry_db import PantryDB
+import sys
+
+
+def main():
+    action = sys.argv[1]
+    item_name = sys.argv[2]
+    category = sys.argv[3]
+    quantity = int(sys.argv[4])
+    expiry_date = sys.argv[5]
+
+    my_pantry = Pantry(PantryDB("pantry.db"))
+    if action == "add":
+        print(f"Adding item: {item_name}, Category: {category}, Expiry: {expiry_date}")
+        my_pantry.add_item(item_name, category, quantity, expiry_date)
+    elif action == "remove":
+        print(
+            f" Trying to remove {quantity} item: {item_name}, Category: {category}, Expiry: {expiry_date}"
+        )
+        my_pantry.add_item(item_name, category, quantity, expiry_date)
+    elif action == "check":
+        print("--------- All Available Items ---------")
+        my_pantry.get_all_pantry_items()
+    else:
+        print("Invalid option, try again")
+
 
 if __name__ == "__main__":
-    my_pantry = Pantry(PantryDB("pantry.db"))
-    my_pantry.add_item("rice 1 kg", "grains", 1, "2025-12-01")
-    my_pantry.add_item("chips", "snacks", 5, "2025-12-01")
-    my_pantry.remove_item("rice 1 kg", "grains", 1, "2025-12-01")
-    my_pantry.add_item("chips - vinegar and salt", "snacks", 2, "2025-12-01")
+    main()
