3724c9f278d2da2bf21207598246a60100bd74e9
rtanoeiro <ramontanoeiro@gmail.com>
2025-03-23 19:01:19 +1100

    ignore db files

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from pantry.pantry import Pantry
 from pantry.pantry_db import PantryDB
 
 if __name__ == "__main__":
-    my_pantry = Pantry(PantryDB(":memory:"))
+    my_pantry = Pantry(PantryDB("pantry.db"))
     results = my_pantry.add_item("rice 1 kg", "grains", "2025-12-01")
     print(results)
