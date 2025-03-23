7372c0596db4fdd9fb68e287ee1d2a546d674009
rtanoeiro <ramontanoeiro@gmail.com>
2025-03-23 18:02:43 +1100

    added new exceptions and validations with them

--- pantry/pantry.py
+++ pantry/pantry.py
@@ -1,7 +1,13 @@
-from pantry.categories import Categories
-from utils.pantry_exceptions import CategoryNotFoundError, ItemNotFoundError
 from datetime import datetime
+
+from pantry.categories import Categories
 from pantry.pantry_db import PantryDB
+from utils.pantry_exceptions import (
+    CategoryNotFoundError,
+    InvalidItemName,
+    InvaliExpiryDate,
+    ItemNotFoundError,
+)
 
 
 class Pantry:
@@ -10,22 +16,14 @@ class Pantry:
         self.pantry_db = pantry_db
 
     def add_item(self, item_name, category, expiry_date):
-        if category not in self.available_categories:
-            raise CategoryNotFoundError(category)
-
-        if not self.validate_date(expiry_date):
-            raise ValueError("Invalid date format. Use YYYY-MM-DD")
+        self.validate_all_inputs(item_name, category, expiry_date)
 
         self.pantry_db.add_item_to_db(
             item_name, category, expiry_date, datetime.today().strftime("%Y-%m-%d")
         )
 
     def remove_item(self, item_name, category, expiry_date):
-        if category not in self.available_categories:
-            raise CategoryNotFoundError(category)
-
-        if not self.validate_date(expiry_date):
-            raise ValueError("Invalid date format. Use YYYY-MM-DD")
+        self.validate_all_inputs(item_name, category, expiry_date)
 
         pantry_items = self.get_pantry_items()
         if not pantry_items:
@@ -41,9 +39,21 @@ class Pantry:
         results = self.pantry_db.check_all_pantry_items()
         return results.fetchall()
 
+    def validate_item_name(self, item_name):
+        if not isinstance(item_name, str):
+            raise InvalidItemName(item_name)
+
+    def validate_category(self, category):
+        if category not in self.available_categories:
+            raise CategoryNotFoundError(category, self.available_categories)
+
     def validate_date(self, date):
         try:
             datetime.strptime(date, "%Y-%m-%d")
-        except ValueError:
-            return False
-        return True
+        except Exception:
+            raise InvaliExpiryDate(date)
+
+    def validate_all_inputs(self, item_name, category, expiry_date):
+        self.validate_item_name(item_name)
+        self.validate_category(category)
+        self.validate_date(expiry_date)
