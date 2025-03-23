02ad03685f4a275f3b4b49104b77eb9555c13fe6
rtanoeiro <ramontanoeiro@gmail.com>
2025-03-23 17:37:44 +1100

    added validation to dates

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from pantry.pantry_db import PantryDB
 
 if __name__ == "__main__":
     my_pantry = Pantry(PantryDB(":memory:"))
-    results = my_pantry.add_item("rice 1 kg", "grains", "2025-12-01")
-    print(results)
+    my_pantry.remove_item("rice 1 kg", "grains", "2025-12-01")
+    items = my_pantry.get_pantry_items()
+    print(items)
