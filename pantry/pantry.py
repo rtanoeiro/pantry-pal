0b0e89cd956a7ed9491e63e99a6e6475c2857317
rtanoeiro <ramontanoeiro@gmail.com>
2025-03-23 17:14:28 +1100

    added pantry class

---
+++ pantry/pantry.py
@@ -0,0 +1,20 @@
+from pantry.categories import Categories
+from utils.pantry_exceptions import CategoryNotFoundError
+from datetime import datetime
+from pantry.pantry_db import PantryDB
+
+
+class Pantry:
+    def __init__(self, pantry_db: PantryDB):
+        self.available_categories = Categories.available_categories()
+        self.pantry_db = pantry_db
+
+    def add_item(self, item_name, category, expiry_date):
+        if category not in self.available_categories:
+            CategoryNotFoundError(category)
+
+        results = self.pantry_db.add_item_to_db(
+            item_name, category, expiry_date, datetime.today().strftime("%Y-%m-%d")
+        )
+
+        return results
