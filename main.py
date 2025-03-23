4279b3c85ec98a346e823d1591204d59aa41e589
rtanoeiro <ramontanoeiro@gmail.com>
2025-03-23 19:51:53 +1100

    main update

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ from pantry.pantry_db import PantryDB
 
 if __name__ == "__main__":
     my_pantry = Pantry(PantryDB("pantry.db"))
-    results = my_pantry.add_item("rice 1 kg", "grains", "2025-12-01")
-    print(results)
+    my_pantry.add_item("rice 1 kg", "grains", 1, "2025-12-01")
+    my_pantry.add_item("chips", "snacks", 5, "2025-12-01")
+    my_pantry.remove_item("rice 1 kg", "grains", 1, "2025-12-01")
+    my_pantry.add_item("chips - vinegar and salt", "snacks", 2, "2025-12-01")
