9b9ba4ebb1c1181328ca21c5ea19981f872ef3e6
rtanoeiro <ramontanoeiro@gmail.com>
2025-03-23 19:03:55 +1100

    updated tests to include quantity

--- tests/test_pantry_pal.py
+++ tests/test_pantry_pal.py
@@ -17,16 +17,17 @@ class TestPantry(unittest.TestCase):
         return self.pantry
 
     def test_add_item(self):
-        self.pantry.add_item("rice 1 kg", "grains", "2025-12-01")
+        self.pantry.add_item("rice 1 kg", "grains", 1, "2025-12-01")
         pantry_items = self.pantry.get_pantry_items()
 
         self.assertEqual(pantry_items[0][0], "rice 1 kg")
         self.assertEqual(pantry_items[0][1], "grains")
+        self.assertEqual(pantry_items[0][2], 1)
         self.assertEqual(pantry_items[0][2], "2025-12-01")
 
     def test_remove_item(self):
-        self.pantry.add_item("rice 1 kg", "grains", "2025-12-01")
-        self.pantry.remove_item("rice 1 kg", "grains", "2025-12-01")
+        self.pantry.add_item("rice 1 kg", "grains", 1, "2025-12-01")
+        self.pantry.remove_item("rice 1 kg", "grains", 1, "2025-12-01")
 
         pantry_items = self.pantry.get_pantry_items()
         self.assertEqual(len(pantry_items), 0)
@@ -37,6 +38,7 @@ class TestPantry(unittest.TestCase):
             self.pantry.remove_item,
             "rice 3 kg",
             "grains",
+            1,
             "2025-12-01",
         )
 
@@ -46,42 +48,48 @@ class TestPantry(unittest.TestCase):
             self.pantry.add_item,
             "rice 1 kg",
             "grain",
+            1,
             "2025-12-01",
         )
 
     def test_add_multiple_items(self):
-        self.pantry.add_item("rice 1 kg", "grains", "2025-12-01")
-        self.pantry.add_item("rice 2 kg", "grains", "2025-12-01")
-        self.pantry.add_item("rice 3 kg", "grains", "2025-12-01")
+        self.pantry.add_item("rice 1 kg", "grains", 1, "2025-12-01")
+        self.pantry.add_item("rice 2 kg", "grains", 2, "2025-12-01")
+        self.pantry.add_item("rice 3 kg", "grains", 1, "2025-12-01")
 
         pantry_items = self.pantry.get_pantry_items()
         self.assertEqual(len(pantry_items), 3)
 
     def test_add_items_different_categories(self):
-        self.pantry.add_item("rice 1 kg", "grains", "2025-12-01")
-        self.pantry.add_item("apple", "fruits", "2025-12-01")
+        self.pantry.add_item("rice 1 kg", "grains", 1, "2025-12-01")
+        self.pantry.add_item("apple", "fruits", 3, "2025-12-01")
 
         pantry_items = self.pantry.get_pantry_items()
         self.assertEqual(len(pantry_items), 2)
 
     def test_add_item_invalid_expiry_date(self):
         self.assertRaises(
-            InvaliExpiryDate, self.pantry.add_item, "rice 1 kg", "grains", "2025-13-01"
+            InvaliExpiryDate,
+            self.pantry.add_item,
+            "rice 1 kg",
+            "grains",
+            2,
+            "2025-13-01",
         )
 
     def test_add_item_invalid_name(self):
         self.assertRaises(
-            InvalidItemName, self.pantry.add_item, None, "grains", "2025-13-01"
+            InvalidItemName, self.pantry.add_item, None, "grains", 1, "2025-13-01"
         )
 
     def test_add_item_invalid_category_none(self):
         self.assertRaises(
-            CategoryNotFoundError, self.pantry.add_item, "rice", None, "2025-13-01"
+            CategoryNotFoundError, self.pantry.add_item, "rice", None, 1, "2025-13-01"
         )
 
     def test_add_item_invalid_date_none(self):
         self.assertRaises(
-            InvaliExpiryDate, self.pantry.add_item, "rice", "grains", None
+            InvaliExpiryDate, self.pantry.add_item, "rice", "grains", 1, None
         )
 
 
