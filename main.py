8350b9eeca33452a5257edf1573ddc0eafef2274
rtanoeiro <ramontanoeiro@gmail.com>
2025-03-23 17:38:39 +1100

    Revert "added validation to dates"
    This reverts commit 02ad03685f4a275f3b4b49104b77eb9555c13fe6.

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ from pantry.pantry_db import PantryDB
 
 if __name__ == "__main__":
     my_pantry = Pantry(PantryDB(":memory:"))
-    my_pantry.remove_item("rice 1 kg", "grains", "2025-12-01")
-    items = my_pantry.get_pantry_items()
-    print(items)
+    results = my_pantry.add_item("rice 1 kg", "grains", "2025-12-01")
+    print(results)
