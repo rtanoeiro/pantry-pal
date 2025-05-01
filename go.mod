0932c36046c2d8b30d4904eacf5a320e4e3dbeca
rtanoeiro <ramontanoeiro@gmail.com>
2025-05-01 21:37:31 +1000

    updated main script with new packages and functions

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module pantry-pal
 
 go 1.24.2
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
+	github.com/tursodatabase/libsql-client-go v0.0.0-20240902231107-85af5b9d094d
+)
+
+require (
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
+	github.com/coder/websocket v1.8.12 // indirect
+	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
+)
--- main.go
+++ main.go
@@ -1,27 +1,56 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"pantry-pal/pantry/api"
+	"pantry-pal/pantry/database"
 
 	"github.com/joho/godotenv"
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
 func main() {
 	envError := godotenv.Load()
-
 	if envError != nil {
 		log.Fatal("Unable to load environment variables")
 	}
 
+	dbURL := os.Getenv("DATABASE_URL")
+	port := os.Getenv("PORT")
+	fmt.Println(dbURL)
+	newDB, dbError := sql.Open("libsql", dbURL)
+	if dbError != nil {
+		log.Fatal("Unable to open database. Closing app. Error: ", dbError)
+	}
+	defer newDB.Close()
+
+	errPing := newDB.Ping()
+	if errPing != nil {
+		log.Fatalf("Failed to ping the database: %v", errPing)
+	}
+
+	config := api.Config{
+		Db:  database.New(newDB),
+		Env: "dev",
+	}
+
+	fmt.Println("Connected to the database successfully")
 	httpServerMux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("static"))
-	httpServerMux.Handle("/", fileServer)
+	baseLoginPage := http.FileServer(http.Dir("static"))
+	httpServerMux.Handle("/", baseLoginPage)
+	httpServerMux.Handle("POST /api/users", http.HandlerFunc(config.CreateUser))
+	httpServerMux.Handle("GET /api/users/{userInfo}", http.HandlerFunc(config.GetUserInfo))
+	httpServerMux.Handle("GET /api/reset", http.HandlerFunc(config.ResetUsers))
 
 	httpServer := http.Server{
 		Handler: httpServerMux,
-		Addr:    ":8080",
+		Addr:    ":" + port,
 	}
-	httpServer.ListenAndServe()
+	fmt.Println("Server started on port", port)
 
+	httpServer.ListenAndServe()
 }
