5965d7b4eeb247eeb4da4f8263ec3bbe27a6de39
rtanoeiro <ramontanoeiro@gmail.com>
2025-05-10 15:22:03 +1000

    removed api from endpoints

--- main.go
+++ main.go
@@ -40,34 +40,33 @@ func main() {
 	httpServerMux := http.NewServeMux()
 
 	// Reset - Used in dev for testing
-	httpServerMux.Handle("POST /api/reset", http.HandlerFunc(config.ResetUsers))
+	httpServerMux.Handle("POST /reset", http.HandlerFunc(config.ResetUsers))
 
 	//Login
-	httpServerMux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "static/login.html")
-	})
-	httpServerMux.HandleFunc("GET /api/signup", func(w http.ResponseWriter, r *http.Request) {
+	httpServerMux.Handle("GET /", http.FileServer(http.Dir("static")))
+	httpServerMux.HandleFunc("GET /signup", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "static/signup.html")
 	})
 
+	// After Login Pages
 	httpServerMux.HandleFunc("GET /home", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "static/home.html")
 	})
-	httpServerMux.Handle("POST /api/login", http.HandlerFunc(config.Login))
+	httpServerMux.Handle("POST /login", http.HandlerFunc(config.Login))
 
 	// Users endpoints
-	httpServerMux.Handle("POST /api/users", http.HandlerFunc(config.CreateUser))
-	httpServerMux.Handle("GET /api/users/", http.HandlerFunc(config.GetUserInfo))
-	httpServerMux.Handle("POST /api/admin", http.HandlerFunc(config.UserAdmin))
-	httpServerMux.Handle("PATCH /api/users/email", http.HandlerFunc(config.UpdateUserEmail))
-	httpServerMux.Handle("PATCH /api/users/name", http.HandlerFunc(config.UpdateUserName))
-	httpServerMux.Handle("PATCH /api/users/password", http.HandlerFunc(config.UpdateUserPassword))
+	httpServerMux.Handle("POST /users", http.HandlerFunc(config.CreateUser))
+	httpServerMux.Handle("GET /users/", http.HandlerFunc(config.GetUserInfo))
+	httpServerMux.Handle("POST /admin", http.HandlerFunc(config.UserAdmin))
+	httpServerMux.Handle("PATCH /users/email", http.HandlerFunc(config.UpdateUserEmail))
+	httpServerMux.Handle("PATCH /users/name", http.HandlerFunc(config.UpdateUserName))
+	httpServerMux.Handle("PATCH /users/password", http.HandlerFunc(config.UpdateUserPassword))
 
 	// Pantry endpoints
-	httpServerMux.Handle("POST /api/pantry", http.HandlerFunc(config.HandleNewItem))
-	httpServerMux.Handle("GET /api/pantry/{itemName}", http.HandlerFunc(config.GetItemByName))
-	httpServerMux.Handle("GET /api/pantry/", http.HandlerFunc(config.GetAllPantryItems))
-	httpServerMux.Handle("DELETE /api/pantry/{itemID}", http.HandlerFunc(config.DeleteItem))
+	httpServerMux.Handle("POST /pantry", http.HandlerFunc(config.HandleNewItem))
+	httpServerMux.Handle("GET /pantry/{itemName}", http.HandlerFunc(config.GetItemByName))
+	httpServerMux.Handle("GET /pantry/", http.HandlerFunc(config.GetAllPantryItems))
+	httpServerMux.Handle("DELETE /pantry/{itemID}", http.HandlerFunc(config.DeleteItem))
 
 	httpServer := http.Server{
 		Handler:           httpServerMux,
