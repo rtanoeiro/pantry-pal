7d3f3f40595fa9753cf74d295bf99d0ef59f9cc3
rtanoeiro <ramontanoeiro@gmail.com>
2025-07-11 23:05:13 +1000

    added base admin user when migrating up

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -12,5 +12,12 @@ CREATE TABLE users (
     UNIQUE(email)
 );
 
+INSERT INTO users (
+    id, name, email, password_hash, created_at, updated_at, is_admin
+)
+VALUES (
+    '4d2ab25a-c902-4015-8ead-09f0e844d42e', 'Admin', 'admin@admin.com', '$2a$10$1ZeLwtMQybfvBA0pzlvA2O.ZY3pW3VjLaYj1kZdNnQI7ZeB3Twr1e', datetime('now'), datetime('now'), 1
+);
+
 -- +goose Down
 DROP TABLE users;
