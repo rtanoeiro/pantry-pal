afafa4c603722c1014a8f771b6eea6f5c176abb7
rtanoeiro <ramontanoeiro@gmail.com>
2025-07-25 23:53:32 +1000

    migrate down to 0

--- scripts/migrate_down.sh
+++ scripts/migrate_down.sh
@@ -2,4 +2,4 @@
 # Trying to set up an env file for the variables above make the scipt fails
 ENV=$1
 
-goose -dir sql/schema sqlite3 ./data/pantry_pal_${ENV}.db down
+goose -dir sql/schema sqlite3 ./data/pantry_pal_${ENV}.db down-to 0
