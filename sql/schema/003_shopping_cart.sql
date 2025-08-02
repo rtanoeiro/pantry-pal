fd72fe17aca06b9533dedcdadf6cb3cd49399867
rtanoeiro <ramontanoeiro@gmail.com>
2025-08-02 14:15:59 +1000

    adjusted shopping cart migrations

--- sql/schema/003_shopping_cart.sql
+++ sql/schema/003_shopping_cart.sql
@@ -2,9 +2,11 @@
 CREATE TABLE shopping_cart (
     id text NOT NULL PRIMARY KEY,
     user_id text NOT NULL,
-    created_at text NOT NULL,
+    created_at timestamp NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id) on DELETE CASCADE
 );
+
+INSERT INTO shopping_cart VALUES ('1', '4d2ab25a-c902-4015-8ead-09f0e844d42e', datetime('now'));
 
 -- +goose Down
 DROP TABLE shopping_cart;
--- sql/schema/004_cart_items.sql
+++ sql/schema/004_cart_items.sql
@@ -4,9 +4,11 @@ CREATE TABLE cart_items (
     item_id text NOT NULL,
     item_name text NOT NULL,
     quantity INTEGER not null,
-    added_at text NOT NULL,
+    added_at timestamp NOT NULL,
     FOREIGN KEY (cart_id) REFERENCES shopping_cart(id) on DELETE CASCADE
 );
+
+INSERT INTO cart_items VALUES ('1', '4d2ab25a-c902', 'Rice', 2, datetime('now'));
 
 -- +goose Down
 DROP TABLE cart_items;
