2b443e8ad6ab1ca5f120355cb5f854c9d47ef32a
rtanoeiro <ramontanoeiro@gmail.com>
2025-08-04 08:26:51 +1000

    shopping cart endpoints update

--- pantry/api/shopping_cart.go
+++ pantry/api/shopping_cart.go
@@ -7,8 +7,7 @@ import (
 	"net/http"
 	"pantry-pal/pantry/database"
 	"strconv"
-
-	"github.com/google/uuid"
+	"time"
 )
 
 func (config *Config) GetAllShopping(writer http.ResponseWriter, request *http.Request) {
@@ -65,13 +64,12 @@ func (config *Config) AddItemShopping(writer http.ResponseWriter, request *http.
 	}
 	currentItem, errFind := config.Db.FindItemShopping(request.Context(), findItem)
 	if errFind == nil {
-		currentItem.Quantity = currentItem.Quantity + int64(itemQuantity)
-		config.UpdateItemShopping(writer, request, currentItem)
+		newQuantity := currentItem.Quantity + int64(itemQuantity)
+		config.UpdateItemShopping(writer, request, newQuantity, currentItem.ItemName, userID)
 		return
 	}
 
 	addItem := database.AddItemShoppingParams{
-		ID:       uuid.New().String(),
 		UserID:   userID,
 		ItemName: itemName,
 		Quantity: int64(itemQuantity),
@@ -86,28 +84,70 @@ func (config *Config) AddItemShopping(writer http.ResponseWriter, request *http.
 
 	writer.WriteHeader(http.StatusOK)
 	cartInfo.SuccessMessage = fmt.Sprintf("Successfully added x%d - %s", addItem.Quantity, addItem.ItemName)
-	_ = config.Renderer.Render(writer, "ResponseMessage", cartInfo)
+	_ = config.Renderer.Render(writer, "shoppingCart", cartInfo)
 }
 
-func (config *Config) UpdateItemShopping(writer http.ResponseWriter, request *http.Request, item database.FindItemShoppingRow) {
-	var returnPantry SuccessErrorResponse
+func (config *Config) AddOneItemShopping(writer http.ResponseWriter, request *http.Request) {
+	var cartInfo SuccessErrorResponse
+	userID, errUser := GetUserIDFromTokenAndValidate(request, config)
+	if errUser != nil {
+		cartInfo.ErrorMessage = fmt.Sprintf("Unable to retrieve user. Error: %s", errUser.Error())
+		writer.WriteHeader(http.StatusInternalServerError)
+		_ = config.Renderer.Render(writer, "ResponseMessage", cartInfo)
+		return
+	}
+	log.Printf("User %s is trying to add one item of %s", userID, request.FormValue("itemName"))
+	request.Form.Add("itemName", request.FormValue("itemName"))
+	request.Form.Add("itemQuantity", "1")
+	config.AddItemShopping(writer, request)
+}
+
+func (config *Config) UpdateItemShopping(writer http.ResponseWriter, request *http.Request, newQuantity int64, itemName, userID string) {
+	var cartInfo SuccessErrorResponse
 	updateItem := database.UpdateItemShoppingParams{
-		Quantity: item.Quantity,
-		ItemName: item.ItemName,
-		UserID:   item.UserID,
+		Quantity: newQuantity,
+		ItemName: itemName,
+		UserID:   userID,
 	}
 	errUpdate := config.Db.UpdateItemShopping(request.Context(), updateItem)
 	if errUpdate != nil {
-		returnPantry.ErrorMessage = "Invalid quantity"
+		cartInfo.ErrorMessage = "Invalid quantity"
 		writer.WriteHeader(http.StatusBadRequest)
-		_ = config.Renderer.Render(writer, "ResponseMessage", returnPantry)
+		_ = config.Renderer.Render(writer, "ResponseMessage", cartInfo)
 		return
 	}
-	returnPantry.SuccessMessage = fmt.Sprintf("Added %d items of %s", updateItem.Quantity, updateItem.ItemName)
+	cartInfo.SuccessMessage = fmt.Sprintf("Added %d items of %s", updateItem.Quantity, updateItem.ItemName)
 	writer.WriteHeader(http.StatusOK)
-	_ = config.Renderer.Render(writer, "ResponseMessage", returnPantry)
+	_ = config.Renderer.Render(writer, "shoppingCart", cartInfo)
 }
 
 func (config *Config) RemoveItemShopping(writer http.ResponseWriter, request *http.Request) {
-	writer.WriteHeader(http.StatusBadRequest)
+	var cartInfo SuccessErrorResponse
+	itemID := request.PathValue("ItemID")
+	userID, errUser := GetUserIDFromTokenAndValidate(request, config)
+	if errUser != nil {
+		log.Printf("Error on deleting item from user %s at %s. Invalid token", userID, time.Now())
+		cartInfo.ErrorMessage = fmt.Sprintf("Unable to retrieve user Pantry Items. Error: %s", errUser.Error())
+		writer.WriteHeader(http.StatusForbidden)
+		_ = config.Renderer.Render(writer, "ResponseMessage", cartInfo)
+		return
+	}
+	log.Printf("User %s is trying to remove %s from shopping cart at %s.", userID, itemID, time.Now())
+
+	removeParams := database.RemoveItemShoppingParams{
+		UserID:   userID,
+		ItemName: userID,
+	}
+	errRemove := config.Db.RemoveItemShopping(request.Context(), removeParams)
+	if errRemove != nil {
+		log.Printf("Error on deleting item %s from user %s shopping cart at %s. Error: %s", removeParams.ItemName, userID, time.Now(), errRemove.Error())
+		cartInfo.ErrorMessage = fmt.Sprintf("Error on deleting item from shopping cart. Error: %s", errRemove.Error())
+		writer.WriteHeader(http.StatusInternalServerError)
+		_ = config.Renderer.Render(writer, "ResponseMessage", cartInfo)
+		return
+	}
+	log.Printf("User %s removed %s from its shopping cart at %s", userID, removeParams.ItemName, time.Now())
+	cartInfo.SuccessMessage = fmt.Sprintf("Successfulyl deleted %s: ", removeParams.ItemName)
+	writer.WriteHeader(http.StatusOK)
+	_ = config.Renderer.Render(writer, "shoppingCart", cartInfo)
 }
