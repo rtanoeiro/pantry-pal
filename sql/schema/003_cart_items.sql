c609ca1e416924a512f58ed153c020b1eb556654
rtanoeiro <ramontanoeiro@gmail.com>
2025-08-04 08:26:22 +1000

    remove id from cart items, as we don't need to track specific ids, but item names

--- sql/schema/003_cart_items.sql
+++ sql/schema/003_cart_items.sql
@@ -1,13 +1,12 @@
 -- +goose Up
 CREATE TABLE cart_items (
-    id text NOT NULL PRIMARY KEY,
     user_id text NOT NULL,
     item_name text NOT NULL,
     quantity INTEGER not null,
     FOREIGN KEY (user_id) REFERENCES users(id) on DELETE CASCADE
 );
 
-INSERT INTO cart_items VALUES ('1', '4d2ab25a-c902-4015-8ead-09f0e844d42e', 'Rice', 2);
+INSERT INTO cart_items VALUES ('4d2ab25a-c902-4015-8ead-09f0e844d42e', 'Rice', 2);
 
 -- +goose Down
 DROP TABLE cart_items;
